c5f5bcf525a75d05406fa8751eba58e22dcd61b7
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-24 10:07:12 -0400

    Implement a method to transform binary bytes to hexidecimals.'

--- XOR.rb
+++ XOR.rb
@@ -17,8 +17,30 @@ def hex_to_bin(input)
     'e' => '1110',
     'f' => '1111',
   }
-
-  hex = input.is_a?(String) ? input : input.to_s(16) 
+  hex = input.is_a?(String) ? input : input.to_s(16)
 
   hex_bin[hex[0]] + hex_bin[hex[1]]
+end
+
+def bin_byte_to_hex(bin_byte)
+  bin_hex = {
+    '0000' => '0',
+    '0001' => '1',
+    '0010' => '2',
+    '0011' => '3',
+    '0100' => '4',
+    '0101' => '5',
+    '0110' => '6',
+    '0111' => '7',
+    '1000' => '8',
+    '1001' => '9',
+    '1010' => 'a',
+    '1011' => 'b',
+    '1100' => 'c',
+    '1101' => 'd',
+    '1110' => 'e',
+    '1111' => 'f',
+  }
+
+  bin_hex[bin_byte[0..3]] + bin_hex[bin_byte[4..7]]
 end
