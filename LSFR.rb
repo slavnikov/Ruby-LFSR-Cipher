65cdeb19e02abb7f9dbb38acbe844ed6206c19e3
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-24 11:43:24 -0400

    Implement an lsfr step method for hexidecimal inputs.

--- LSFR.rb
+++ LSFR.rb
@@ -2,6 +2,17 @@ require_relative 'XOR'
 
 def right_shift_hex(hex, offset)
   binary = hex_to_bin(hex)
+  shifted_binary  = '0' * offset + binary[0...-1 * offset]
 
-  '0' * offset + binary[0...-1 * offset]
+  bin_to_hex(shifted_binary)
+end
+
+def lsfr_step_hex(current_hex, feedback_hex)
+  current_binary = hex_to_bin(current_hex)
+
+  if current_binary[-1] == '0'
+    right_shift_hex(current_hex, 1)
+  else
+    hex_xor(right_shift_hex(current_hex, 1), feedback_hex)
+  end
 end
