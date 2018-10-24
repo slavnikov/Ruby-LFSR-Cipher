29b61bfe141fb317554879b70ab69ec0508eddb4
Dmitry Slavnikov <dmitry.slavnikov@gmail.com>
2018-10-24 12:05:17 -0400

    Implement a method to gather the next lsft key.

--- LSFR.rb
+++ LSFR.rb
@@ -16,3 +16,14 @@ def lsfr_step_hex(current_hex, feedback_hex)
     hex_xor(right_shift_hex(current_hex, 1), feedback_hex)
   end
 end
+
+def next_lsfr_hex_key(current_hex, feedback_hex)
+  8.times do
+    current_hex = lsfr_step(current_hex, feedback_hex)
+  end
+  current_hex[-2..-1]
+end
+
+def char_to_hex(char)
+  char.ord.to_s(16)
+end
